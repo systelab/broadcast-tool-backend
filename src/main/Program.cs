22b11c8db34c9d77c0eae56116abd7ae888307f4
Aritz Peñalver <apenalver@werfen.com>
2018-08-25 10:00:40 +0200

    Program.cs with initial seed

--- src/main/Program.cs
+++ src/main/Program.cs
@@ -8,24 +8,24 @@
     {
         public static void Main(string[] args)
         {
-            //var host = BuildWebHost().Seed();
+            var host = BuildWebHost().Seed();
 
-            //host.Run();
-            BuildWebHost(args).Run();
+            host.Run();
+            // BuildWebHost(args).Run();
         }
 
-        //private static IWebHost BuildWebHost()
-        //{
-        //    return new WebHostBuilder()
-        //        .UseKestrel()
-        //        .UseContentRoot(Directory.GetCurrentDirectory()).UseUrls("http://0.0.0.0:13080")
-        //        .UseIISIntegration()
-        //        .UseStartup<Startup>()
-        //        .Build();
-        //}
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        private static IWebHost BuildWebHost()
+        {
+            return new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory()).UseUrls("http://0.0.0.0:13080")
+                .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
+        }
+        //public static IWebHost BuildWebHost(string[] args) =>
+        //    WebHost.CreateDefaultBuilder(args)
+        //        .UseStartup<Startup>()
+        //        .Build();
     }
 }
